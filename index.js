8edcc3cf16023638eafdabe414acf54907f463db
Jordan Huitema <Jordan-Huitema@live.nmit.ac.nz>
2020-05-05 11:02:53 +1200

    end lesson start lab

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ console.log((-13.55123).toFixed(3));
 console.log((5316).toLocaleString('ar'));
 console.log(('44453').toLocaleString('ar'));
 
-
+//diffrent functions
 function example1 (x) {console.log(x);};
 example1('Im a Func');
 
@@ -67,28 +67,95 @@ example2('Im a Func');
 (x => {console.log(x);})
     ('So am I');;
 
-//callback functions
-function callback1(x) {
+//callback or high order function
+function callback1 (x) {
     console.log(x);
 }
-function callback2(y) {
-    let x = 'dang i been called back'
-    y(x)
+
+function callback2 (y) {
+    y('dang i been called back');
 }
-callback2(callback1)
+callback2(callback1);
 
-console.log("Hello World".length)
+console.log("Hello World".length);
 
-let text1 = '           first-name      '
-let text2 = 'last-name'
-let fullName = (text1.concat(" " + text2))
+let text1 = '           first-name      ';
+let text2 = 'last-name';
+let fullName = (text1.concat(" " + text2));
 
-console.log(fullName)
-console.log(fullName.toUpperCase())
-console.log(text1.trim())
+console.log(fullName);
+console.log(fullName.toUpperCase());
+console.log(text1.trim());
 
-let stringObj = new String('String')
-console.log(stringObj)
+let stringObj = new String('String');
+console.log(stringObj);
 
-let backToString = stringObj.toString()
-console.log(backToString)
+let backToString = stringObj.toString();
+console.log(backToString);;
+
+// ------------Class 2
+
+let students = ['Dale', 'Frank', 'Kyran', 'Aron']
+
+students.forEach(function (eachStudent, index){
+    console.log(index + 1 + "." + eachStudent)
+})
+
+let myObject = {
+    firstName: 'Ali',
+    lastName: 'Kahwaji'
+}
+
+console.log(myObject)
+
+console.log(myObject.lastName)
+myObject.name = 'Muaz'
+console.log(myObject.name)
+
+//rules for objs
+
+//Only use letters, digits, underscores
+//Must start with a letter or underscore
+//Names are case-sensitive
+//Must not match a reserved keyword in JS
+
+students = {
+    name:   'Luke',
+    class:  'SDV503',
+    school: 'NMIT'
+}
+
+console.log(Object.keys(students))
+
+// Object.freeze(students)
+students.name = 'Phil'
+console.log(students)
+console.log(Object.isFrozen(students))
+
+let anotherStudent = {
+    studentName:    'Luke',
+    otherClass:     'CSA',
+    school:         'MIT'
+}
+
+let newObj = Object.assign(students, anotherStudent)
+console.log(newObj)
+
+// Object.create()
+
+let isThis = { 
+    one: 1,
+    two: 3
+};
+let isThat = { 
+    one: 1,
+    two: 2
+};
+console.log(Object.is(isThis.one,isThat.one))
+console.log(Object.is(isThis.two,isThat.two))
+// Object.is()
+
+let exObj = {}
+Object.freeze(exObj)
+console.log(Object.isFrozen(exObj))
+console.log(Object.isExtensible(exObj))
